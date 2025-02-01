a0a010ab0fe462bde58c04da089baf94a360ee06
sander de snaijer <sanderdesnaijer@gmail.com>
2025-02-01 15:54:58 +0700

    added test

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,9 +1,11 @@
 export const convertDate = (date: string, shouldShowDay = true) => {
-  return new Date(date).toLocaleDateString("en-GB", {
+  const format: Intl.DateTimeFormatOptions = {
     ...(shouldShowDay && {
       day: "numeric",
     }),
     month: "short",
     year: "numeric",
-  });
+  };
+
+  return new Date(date).toLocaleDateString("en-GB", format);
 };
