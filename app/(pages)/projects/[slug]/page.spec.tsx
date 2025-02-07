1dfde66f2f1534ecd5b4facc653f5a9cdecb8c3d
sander de snaijer <sanderdesnaijer@gmail.com>
2025-02-07 16:48:35 +0700

    fix tests

--- app/(pages)/projects/[slug]/page.spec.tsx
+++ app/(pages)/projects/[slug]/page.spec.tsx
@@ -3,10 +3,7 @@ import { sanityFetch } from "@/sanity/lib/fetch";
 import ProjectPage from "./page";
 import { mockProject } from "@/app/test-utils/mockProjects";
 import { getTranslationKey } from "@/app/test-utils/i18n";
-
-jest.mock("@/components/Project", () => ({
-  Project: jest.fn(() => <div>Mocked Project Component</div>),
-}));
+import { mockSetting } from "@/app/test-utils/mockSetting";
 
 describe("app/(pages)/[slug]/page", () => {
   afterEach(() => {
@@ -16,6 +13,7 @@ describe("app/(pages)/[slug]/page", () => {
   describe("ProductPage Component", () => {
     it("renders the Project component with fetched project data", async () => {
       (sanityFetch as jest.Mock).mockResolvedValueOnce(mockProject);
+      (sanityFetch as jest.Mock).mockResolvedValueOnce(mockSetting);
 
       const params = { slug: "test-project" };
 
@@ -25,11 +23,12 @@ describe("app/(pages)/[slug]/page", () => {
         query: expect.any(String),
         params,
       });
-      expect(screen.getByText("Mocked Project Component")).toBeInTheDocument();
+      expect(screen.getByText(mockProject.title)).toBeInTheDocument();
     });
 
     it("renders PageNotFound when project data is not found", async () => {
       (sanityFetch as jest.Mock).mockResolvedValueOnce(null);
+      (sanityFetch as jest.Mock).mockResolvedValueOnce(mockSetting);
 
       const params = { slug: "nonexistent-project" };
 
