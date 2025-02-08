e3a24395fa0c89a0ebbb835537a5c1acd48d6a85
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-02-08 17:35:27 +0700

    54 add contract in job title for about page (#55)
    * added employment type
    
    * sorting in backend and added reference

--- sanity/schemaTypes/jobType.tsx
+++ sanity/schemaTypes/jobType.tsx
@@ -10,21 +10,40 @@ export const jobType = defineType({
     defineField({
       name: "companyName",
       type: "string",
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: "jobTitle",
       type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "employmentType",
+      title: "Employment Type",
+      type: "string",
+      options: {
+        list: [
+          { title: "Full-Time", value: "full-time" },
+          { title: "Intern", value: "intern" },
+          { title: "Contract", value: "contract" },
+        ],
+        layout: "dropdown",
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "contractName",
+      title: "Contract Name",
+      type: "reference",
+      to: [{ type: "job" }],
+      hidden: ({ parent }) => parent?.employmentType !== "contract",
+      options: {
+        disableNew: true,
+      },
     }),
     defineField({
       name: "logo",
       type: "image",
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: "description",
-      type: "blockContent",
     }),
     defineField({
       name: "startDate",
@@ -33,6 +52,10 @@ export const jobType = defineType({
     defineField({
       name: "endDate",
       type: "datetime",
+    }),
+    defineField({
+      name: "description",
+      type: "blockContent",
     }),
     defineField({
       name: "tags",
@@ -51,4 +74,16 @@ export const jobType = defineType({
       media: "logo",
     },
   },
+  orderings: [
+    {
+      title: "Start Date (Newest First)",
+      name: "startDateDesc",
+      by: [{ field: "startDate", direction: "desc" }],
+    },
+    {
+      title: "Start Date (Oldest First)",
+      name: "startDateAsc",
+      by: [{ field: "startDate", direction: "asc" }],
+    },
+  ],
 });
