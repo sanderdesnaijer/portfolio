59672aa46de2a1b0e4e826cccd6352e277e13d19
sander de snaijer <sanderdesnaijer@gmail.com>
2025-03-03 22:18:32 +0700

    added italic hover on project overview

--- app/components/ProjectListItem.tsx
+++ app/components/ProjectListItem.tsx
@@ -40,7 +40,7 @@ export const ProjectListItem = ({
     <div className="col-span-4 px-4 md:col-span-3">
       <Link
         href={href}
-        className="no-underline before:absolute before:right-0 before:left-0 before:h-full before:opacity-0"
+        className="no-underline group-hover/item:italic before:absolute before:right-0 before:left-0 before:h-full before:opacity-0"
       >
         <h2
           className={`-mt-2 mb-0 text-3xl leading-10 md:text-[2.5rem] md:leading-11 ${getChevronClasses()} after:mt-0.5 after:h-11 after:w-11`}
