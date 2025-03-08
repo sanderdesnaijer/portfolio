fab66b427b14c2ac226735290209d4956b06e7ea
Sander de Snaijer <sander.de.snaijer@knmi.nl>
2025-03-08 20:09:17 +0700

    added hovers on project list and about

--- app/components/ProjectListItem.tsx
+++ app/components/ProjectListItem.tsx
@@ -27,14 +27,16 @@ export const ProjectListItem = ({
   <li className="group/item transition-del relative mt-0 mb-0 grid grid-cols-5 justify-between pl-0 no-underline transition-opacity duration-200">
     <div className="col-span-1 md:col-span-2">
       {imageURL && imageALT && (
-        <Image
-          className="mt-0 object-fill"
-          src={imageURL}
-          alt={imageALT}
-          width={350}
-          height={350}
-          priority
-        />
+        <div className="inline-block overflow-hidden">
+          <Image
+            className="mt-0 mb-0 scale-100 object-fill transition group-hover:blur-[2px] group-hover/item:scale-105 group-hover/item:blur-none"
+            src={imageURL}
+            alt={imageALT}
+            width={350}
+            height={350}
+            priority
+          />
+        </div>
       )}
     </div>
     <div className="col-span-4 px-4 md:col-span-3">
@@ -43,7 +45,7 @@ export const ProjectListItem = ({
         className="no-underline group-hover/item:italic before:absolute before:right-0 before:left-0 before:h-full before:opacity-0"
       >
         <h2
-          className={`-mt-2 mb-0 text-3xl leading-10 md:text-[2.5rem] md:leading-11 ${getChevronClasses()} after:mt-0.5 after:h-11 after:w-11`}
+          className={`-mt-2 mb-0 translate-x-0 text-3xl leading-10 transition-transform group-hover/item:translate-x-2 md:text-[2.5rem] md:leading-11 ${getChevronClasses()} after:mt-0.5 after:h-11 after:w-11`}
         >
           {title}
         </h2>
