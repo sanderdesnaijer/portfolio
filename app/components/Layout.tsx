147678b4bf397886e6b850f365650d71df1f5970
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-03-16 22:34:15 +0700

    fix flicker, body padding, toggle animation on off

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -88,7 +88,7 @@ export const Layout: React.FC<{
         ref={mainRef}
         className="group peer relative top-[0px] z-20 col-span-9 flex flex-col justify-end md:sticky md:top-0 md:col-span-2 md:h-screen md:flex-row md:gap-4 md:px-6 md:py-0 md:pb-6 xl:px-0"
       >
-        <div className="flex flex-col justify-between bg-white px-6 py-2 group-[.sticky]:-translate-y-full group-[.sticky-show]:translate-y-0 group-[.sticky-show]:shadow-md group-[.sticky-transition]:transition-all md:items-center md:bg-transparent md:px-0 md:py-0 dark:bg-black dark:group-[.sticky-show]:shadow-[0px_2px_4px_-2px_rgba(0,0,0,0.8)]">
+        <div className="flex flex-col justify-between px-6 py-2 group-[.sticky]:-translate-y-full group-[.sticky]:bg-white group-[.sticky-show]:translate-y-0 group-[.sticky-show]:shadow-md group-[.sticky-transition]:transition-transform md:items-center md:bg-transparent md:px-0 md:py-0 dark:group-[.sticky]:bg-black dark:group-[.sticky-show]:shadow-[0px_2px_4px_-2px_rgba(0,0,0,0.8)]">
           <nav
             aria-label="author-navigation"
             className="order-2 flex justify-between md:block"
@@ -107,7 +107,7 @@ export const Layout: React.FC<{
         </div>
         <Menu ref={navRef} menuItems={menuItems} />
       </div>
-      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pt-6 peer-[.sticky]:top-[24px] peer-[.sticky]:pb-28 md:col-span-5 md:px-0 md:pt-24">
+      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pt-6 peer-[.sticky]:top-[24px] md:col-span-5 md:px-0 md:pt-24">
         <div className="flex h-full flex-col">
           <h1 className="relative text-5xl font-bold after:absolute after:right-0 after:-bottom-5 after:-left-10 after:h-px after:w-[100vw] after:bg-current md:mb-10 md:text-8xl md:after:-bottom-10 md:after:left-[-196px] after:dark:bg-white">
             {pageTitle}
--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -15,7 +15,7 @@ const Menu = React.forwardRef<
 >(
   (
     {
-      className = "order-2 group-[.sticky]:translate-y-full group-[.sticky-show]:translate-y-0 group-[.sticky-transition]:transition-transform flex min-w-[172px] border-none px-6 font-extralight group-[.sticky]:fixed group-[.sticky]:right-0 group-[.sticky]:bottom-0 group-[.sticky]:left-0 group-[.sticky]:place-content-evenly group-[.sticky]:bg-white group-[.sticky]:py-8 group-[.sticky]:text-2xl group-[.sticky]:shadow-[0_-4px_6px_-1px_var(--tw-shadow-color,rgb(0_0_0_/_0.1))] md:flex-col md:justify-end md:pl-4 md:text-4xl dark:group-[.sticky]:bg-black dark:group-[.sticky]:shadow-[0px_-2px_4px_-2px_rgba(0,0,0,0.8)]",
+      className = "order-2 group-[.sticky]:translate-y-full group-[.sticky-show]:translate-y-0 group-[.sticky-transition]:transition-transform flex min-w-[172px] border-none px-6 group-[.sticky]:px-0 font-extralight group-[.sticky]:fixed group-[.sticky]:right-0 group-[.sticky]:bottom-0 group-[.sticky]:left-0 group-[.sticky]:place-content-evenly group-[.sticky]:bg-white group-[.sticky]:py-0 group-[.sticky]:text-2xl group-[.sticky]:shadow-[0_-4px_6px_-1px_var(--tw-shadow-color,rgb(0_0_0_/_0.1))] md:flex-col md:justify-end md:pl-4 md:text-4xl dark:group-[.sticky]:bg-black dark:group-[.sticky]:shadow-[0px_-2px_4px_-2px_rgba(0,0,0,0.8)]",
       menuItems,
     },
     ref
@@ -36,7 +36,7 @@ const Menu = React.forwardRef<
       <nav ref={ref} aria-label="page-navigation" className={className}>
         {menuItems.map((item, i) => (
           <Link
-            className={`${isActive(item) ? "font-bold" : ""} mr-2 translate-x-0 transition hover:font-bold hover:italic md:mr-auto md:hover:translate-x-2`}
+            className={`${isActive(item) ? "font-bold" : ""} mr-2 translate-x-0 transition-transform group-[.sticky]:mr-0 group-[.sticky]:w-1/4 group-[.sticky]:pt-4 group-[.sticky]:pb-6 group-[.sticky]:text-center hover:font-bold hover:italic focus:font-bold active:font-bold md:mr-auto md:hover:translate-x-2`}
             key={i}
             href={item.pathname}
           >
