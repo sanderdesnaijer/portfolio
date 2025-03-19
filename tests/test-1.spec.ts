88c22d2d2c0906be526602119f1b4d705261ad4c
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-03-20 00:14:05 +0700

    fix env

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  if (!process.env.NEXT_PUBLIC_BASE_URL!) {
+    throw new Error("NEXT_PUBLIC_BASE_URL is missing");
+  }
+  await page.goto(process.env.NEXT_PUBLIC_BASE_URL);
 
   await page.getByRole("link", { name: "Projects" }).click();
   await page.getByRole("link", { name: "Flutter Tabata whip timer" }).click();
