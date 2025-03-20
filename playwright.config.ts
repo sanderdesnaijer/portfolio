a20b5fea15281e2776bbad2bf534cd4f1784c614
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-03-20 11:37:25 +0700

    base url and webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,10 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 
+if (!process.env.NEXT_PUBLIC_BASE_URL!) {
+  throw new Error("NEXT_PUBLIC_BASE_URL is missing");
+}
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,9 +75,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    // url: 'http://127.0.0.1:3000',
+    port: 3000,
+    timeout: 120 * 1000, // Give it time to start
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,11 +1,7 @@
 import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  if (!process.env.NEXT_PUBLIC_BASE_URL!) {
-    throw new Error("NEXT_PUBLIC_BASE_URL is missing");
-  }
-  await page.goto(process.env.NEXT_PUBLIC_BASE_URL);
-
+  await page.goto("/");
   await page.getByRole("link", { name: "Projects" }).click();
   await page.getByRole("link", { name: "Flutter Tabata whip timer" }).click();
 });
