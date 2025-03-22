bff94519662d52ec8ce019a68f7ef42e17e22036
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-03-22 23:42:39 +0700

    cleanup

--- app/api/pageData/utils.ts
+++ app/api/pageData/utils.ts
@@ -1,14 +1,12 @@
 import { REVALIDATE_INTERVAL } from "@/app/utils/constants";
+import { getBaseUrl } from "@/app/utils/routes";
 import { PageSanity } from "@/sanity/types";
 
 export async function fetchPageData(slug = ""): Promise<PageSanity | null> {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/pageData?slug=${slug}`,
-      {
-        next: { revalidate: REVALIDATE_INTERVAL },
-      }
-    );
+    const res = await fetch(`${getBaseUrl()}/api/pageData?slug=${slug}`, {
+      next: { revalidate: REVALIDATE_INTERVAL },
+    });
     if (!res.ok) throw new Error("Failed to fetch page");
     const data = await res.json();
     return data;
--- app/api/project/utils.ts
+++ app/api/project/utils.ts
@@ -1,16 +1,14 @@
 import { REVALIDATE_INTERVAL } from "@/app/utils/constants";
+import { getBaseUrl } from "@/app/utils/routes";
 import { ProjectTypeSanity } from "@/sanity/types";
 
 export async function fetchProjectData(
   slug = ""
 ): Promise<ProjectTypeSanity | null> {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/project?slug=${slug}`,
-      {
-        next: { revalidate: REVALIDATE_INTERVAL },
-      }
-    );
+    const res = await fetch(`${getBaseUrl()}/api/project?slug=${slug}`, {
+      next: { revalidate: REVALIDATE_INTERVAL },
+    });
 
     if (!res.ok) throw new Error("Failed to fetch page");
     const data = await res.json();
