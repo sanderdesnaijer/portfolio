6d0ef125ba1e6149888d3258364704cafc5180ff
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-03-27 21:39:45 +0700

    fixed tablet viewport

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -22,7 +22,7 @@ export const Layout: React.FC<{
     <div className="mx-auto grid grid-cols-9 pt-6 md:container md:pt-0">
       <div
         ref={stickyRef}
-        className="group peer relative top-[0px] z-20 col-span-9 flex flex-col justify-end md:sticky md:top-0 md:col-span-2 md:h-screen md:flex-row md:gap-4 md:px-6 md:py-0"
+        className="group peer relative top-[0px] z-20 col-span-9 flex flex-col justify-end md:sticky md:top-0 md:col-span-3 md:h-screen md:flex-row md:gap-4 md:px-6 md:py-0 xl:col-span-2"
       >
         <Menu menuItems={menuItems} />
         <header className="flex flex-col justify-between px-6 py-2 group-[.sticky]:-translate-y-full group-[.sticky]:bg-white group-[.sticky-show]:translate-y-0 group-[.sticky-show]:shadow-md group-[.sticky-transition]:transition-transform md:items-center md:overflow-visible md:bg-transparent md:px-0 md:py-6 dark:group-[.sticky]:bg-black dark:group-[.sticky-show]:shadow-[0px_2px_4px_-2px_rgba(0,0,0,0.8)]">
@@ -40,7 +40,7 @@ export const Layout: React.FC<{
           </div>
         </header>
       </div>
-      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pt-6 peer-[.sticky]:top-[24px] md:col-span-5 md:px-0 md:pt-24">
+      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pt-6 peer-[.sticky]:top-[24px] md:col-span-6 md:px-0 md:pt-24 lg:col-span-5">
         <div className="flex h-full flex-col">
           <h1 className="relative text-5xl font-bold after:absolute after:right-0 after:-bottom-5 after:-left-10 after:h-px after:w-[100vw] after:bg-current md:mb-10 md:text-8xl md:after:-bottom-10 md:after:left-[-196px] after:dark:bg-white">
             {pageTitle}
