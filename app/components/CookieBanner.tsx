8a4fc11ea6e099a8033816404cd6f6384c2e0c52
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-05 13:15:30 +0700

    cookie banner responsive

--- app/components/CookieBanner.tsx
+++ app/components/CookieBanner.tsx
@@ -42,7 +42,7 @@ export default function CookieBanner() {
 
   return (
     <div
-      className={`fixed right-0 bottom-0 left-0 z-20 mx-auto my-10 flex max-w-max flex-col items-center justify-between gap-4 rounded-lg bg-gray-700 px-3 py-3 shadow sm:flex-row md:max-w-screen-sm md:px-4`}
+      className={`fixed right-0 bottom-0 left-0 z-20 m-3 flex max-w-3xl flex-col items-center justify-between rounded-lg bg-gray-700 px-3 py-3 shadow md:mx-auto md:flex-row md:px-4`}
     >
       <div className="text-center">
         <Link href="/info/cookies">
@@ -50,7 +50,7 @@ export default function CookieBanner() {
         </Link>
       </div>
 
-      <div className="flex gap-2">
+      <div className="flex flex-col gap-2 md:flex-row">
         <button
           onClick={() => onChangeConsent(false)}
           className="rounded-md border-gray-900 px-5 py-2 text-gray-300"
@@ -59,7 +59,7 @@ export default function CookieBanner() {
         </button>
         <button
           onClick={() => onChangeConsent(true)}
-          className="rounded-lg bg-gray-900 px-5 py-2 whitespace-nowrap text-white"
+          className="cursor-pointer rounded-lg bg-blue-500 px-5 py-2 whitespace-nowrap text-white hover:bg-blue-600"
         >
           {t("accept")}
         </button>
