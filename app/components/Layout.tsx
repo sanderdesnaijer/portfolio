056e340e6e547ca91f6fe9424e44a3994188b044
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-06 17:16:36 +0700

    fixed paddings

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -47,12 +47,12 @@ export const Layout: React.FC<{
           </div>
         </div>
       </header>
-      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pb-12 md:col-span-6 md:px-0 md:pt-6 md:pb-0 lg:col-span-5">
+      <main className="prose prose-xl dark:prose-invert relative col-span-9 max-w-fit px-6 pb-6 md:col-span-6 md:px-0 md:pt-6 md:pb-0 lg:col-span-5">
         <div className="flex flex-col">
           <h1 className="relative text-5xl font-bold after:absolute after:right-0 after:-bottom-5 after:-left-10 after:h-px after:w-[100vw] after:bg-current md:my-10 md:text-8xl md:after:-bottom-10 md:after:left-[-196px] after:dark:bg-white">
             {pageTitle}
           </h1>
-          <div className="relative flex-1 after:absolute after:top-0 after:right-0 after:bottom-0 after:left-[-196px] after:w-px after:bg-black md:pt-0 dark:after:bg-white">
+          <div className="relative flex-1 after:absolute after:top-0 after:right-0 after:bottom-0 after:left-[-196px] after:w-px after:bg-black md:pt-0 md:pb-6 dark:after:bg-white">
             {children}
           </div>
         </div>
