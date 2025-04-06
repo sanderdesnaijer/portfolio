89133a3c9ecf0d6fb1f04e6cffdcaf311c68ba95
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-06 21:58:38 +0700

    increase timeout

--- e2e/tests/ui/mobile-navigation.spec.ts
+++ e2e/tests/ui/mobile-navigation.spec.ts
@@ -27,7 +27,7 @@ test.describe("mobile navigation", () => {
 
     // Scroll down 500px
     await page.evaluate(() => window.scrollBy(0, 500));
-    await page.waitForTimeout(500); // Ensure scroll event processes
+    await page.waitForTimeout(1000); // Ensure scroll event processes
 
     // Check that navigation is NOT in viewport (hidden)
     await expect(nav).not.toBeInViewport();
