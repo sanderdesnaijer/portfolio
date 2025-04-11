cd8d29a364d05b1d173caed3b11d958f456580f2
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-11 11:54:13 +0700

    remove unused type

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,8 +1,6 @@
 import { formats } from "@/i18n/request";
 import en from "./translations/en.json";
 
-type Messages = typeof en;
-
 declare global {
   interface Window {
     gtag?: (...args: unknown[]) => void;
