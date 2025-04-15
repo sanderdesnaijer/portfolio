95018a343d7a58b2acf0ce94003e2aa9ec93b51e
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-15 11:39:31 +0700

    more heading padding mobile

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -3,7 +3,7 @@ export const PageLayout: React.FC<{
   children: React.ReactNode;
 }> = ({ title, children }) => (
   <>
-    <h1 className="relative text-5xl font-bold after:absolute after:right-0 after:-bottom-5 after:-left-10 after:h-px after:w-[100vw] after:bg-current md:my-10 md:text-8xl md:after:-bottom-10 md:after:left-[-196px] after:dark:bg-white">
+    <h1 className="relative mt-3 text-5xl font-bold after:absolute after:right-0 after:-bottom-5 after:-left-10 after:h-px after:w-[100vw] after:bg-current md:my-10 md:text-8xl md:after:-bottom-10 md:after:left-[-196px] after:dark:bg-white">
       {title}
     </h1>
     <div className="relative flex-1 after:absolute after:top-0 after:right-0 after:bottom-0 after:left-[-196px] after:w-px after:bg-black md:pt-0 md:pb-6 dark:after:bg-white">
