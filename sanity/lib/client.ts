9e5f0e99e03e69e5e1e5a71aea99e91a86272f1b
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-16 17:47:56 +0700

    added blog type

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from "next-sanity";
 
 import { apiVersion, dataset, projectId } from "../env";
+import envConfig from "@/envConfig";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
+  token: envConfig.sanityToken,
   useCdn: process.env.NODE_ENV === "production", // Use CDN in production
 });
