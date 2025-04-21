7ef776b295bb9704e1101efb8e9c0c0a7ce9b8cc
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-04-21 13:18:48 +0700

    some tweaks

--- app/utils/useScrollPosition.ts
+++ app/utils/useScrollPosition.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 interface UseScrollHeaderOptions {
   className?: string;
@@ -16,58 +16,72 @@ export const useScrollPosition = (
     mediaQueryMatch = "(min-width: 48rem)",
   }: UseScrollHeaderOptions = {}
 ) => {
+  const scrollDirection = useRef("");
+  const lastY = useRef(0);
+  const isPastOffset = useRef(false);
+  const offset = useRef(0);
+
   useEffect(() => {
-    const element = headerRef.current;
-    if (!element) return;
-
-    const offsetElement = element.querySelector(offsetSelector);
-    if (!offsetElement) {
-      // eslint-disable-next-line no-console
-      console.warn(`No element found for selector "${offsetSelector}"`);
-    }
-
-    const offset = offsetElement?.getBoundingClientRect().height || 0;
     const mediaQuery = window.matchMedia(mediaQueryMatch);
 
-    let direction = "";
-    let lastY: number = 0;
-    let isPastOffset = false;
-    let isDisabled = mediaQuery.matches;
-
     const onScroll = () => {
-      if (isDisabled) return;
+      if (!headerRef.current) return;
 
       const { scrollY } = window;
 
-      isPastOffset = scrollY > offset;
+      isPastOffset.current = scrollY > offset.current;
 
       const isAtBottom =
         scrollY + window.innerHeight >= document.documentElement.scrollHeight;
-
       if (!isAtBottom) {
-        direction = scrollY > lastY ? "down" : "up";
+        scrollDirection.current = scrollY > lastY.current ? "down" : "up";
       }
 
-      element.classList.toggle(shadowClassName, scrollY > 0);
-      element.classList.toggle(className, isPastOffset && direction === "down");
+      headerRef.current.classList.toggle(shadowClassName, scrollY > 0);
+      headerRef.current.classList.toggle(
+        className,
+        isPastOffset.current && scrollDirection.current === "down"
+      );
 
-      lastY = scrollY;
+      lastY.current = scrollY;
+    };
+
+    const getOffset = () => {
+      const offsetElement = headerRef.current?.querySelector(offsetSelector);
+      if (!offsetElement) {
+        console.warn(`No element found for selector "${offsetSelector}"`);
+        return 0;
+      }
+      return offsetElement.getBoundingClientRect().height;
+    };
+
+    const enableScrollHandling = () => {
+      offset.current = getOffset();
+      window.addEventListener("scroll", onScroll, { passive: true });
+    };
+
+    const destroyScrollHandling = () => {
+      window.removeEventListener("scroll", onScroll);
+      headerRef.current?.classList.remove(className, shadowClassName);
     };
 
     const onMediaChange = (event: MediaQueryListEvent) => {
-      isDisabled = event.matches;
       if (event.matches) {
-        element.classList.remove(className, shadowClassName);
+        destroyScrollHandling();
+      } else {
+        enableScrollHandling();
       }
     };
 
+    if (!mediaQuery.matches) {
+      enableScrollHandling();
+    }
+
     mediaQuery.addEventListener("change", onMediaChange);
-    window.addEventListener("scroll", onScroll, { passive: true });
 
     return () => {
-      window.removeEventListener("scroll", onScroll);
       mediaQuery.removeEventListener("change", onMediaChange);
-      element.classList.remove(className, shadowClassName);
+      destroyScrollHandling();
     };
   }, [headerRef, className, offsetSelector, mediaQueryMatch, shadowClassName]);
 };
