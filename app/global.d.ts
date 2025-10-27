811434cc1e90771b9a5a0bcdae1a84e6fb97145f
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-10-27 20:48:13 +0700

    added svg defs

--- app/global.d.ts
+++ app/global.d.ts
@@ -13,12 +13,15 @@ declare module "next-intl" {
     Formats: typeof formats;
   }
 }
-
-// SVG module declarations
 declare module "*.svg" {
   import * as React from "react";
   const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >;
   export default ReactComponent;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
 }
