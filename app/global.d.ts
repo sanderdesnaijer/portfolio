cec14a1a388861cfa10bc1bb6ad802903a4a3f19
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-10-28 13:07:14 +0700

    move types

--- app/global.d.ts
+++ app/global.d.ts
@@ -13,3 +13,26 @@ declare module "next-intl" {
     Formats: typeof formats;
   }
 }
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  export default ReactComponent;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg?inline" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
