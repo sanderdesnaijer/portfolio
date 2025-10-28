d826ca3ed8b01e8ca5bd438f6249d9f9bb5739fc
Sander de Snaijer <sanderdesnaijer@gmail.com>
2025-10-28 13:20:05 +0700

    svg test

--- app/types/svg.d.ts
+++ app/types/svg.d.ts
@@ -25,106 +25,106 @@ declare module "*.svg?inline" {
 }
 
 // Explicit declarations for specific SVG files
-declare module "@/app/assets/logo.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/logo.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/github.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/github.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/article.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/article.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/download.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/download.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/demo.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/demo.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/gitlab.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/gitlab.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/linkedin.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/linkedin.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/missing.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/missing.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/chevron-right.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/chevron-right.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/x.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/x.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/youtube.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/youtube.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/instagram.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/instagram.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
 
-declare module "@/app/assets/icons/medium.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
+// declare module "@/public/icons/medium.svg" {
+//   import * as React from "react";
+//   const ReactComponent: React.FunctionComponent<
+//     React.SVGProps<SVGSVGElement> & { title?: string }
+//   >;
+//   export default ReactComponent;
+// }
