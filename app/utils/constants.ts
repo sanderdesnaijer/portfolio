1612bc98b84eab22ed86892066e023af33b13034
Sander de Snaijer <sanderdesnaijer@gmail.com>
2026-05-03 16:42:24 +0700

    fix: revalidate

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,3 +1,3 @@
 export const AUTHOR_NAME = "Sander de Snaijer";
 export const AUTHOR_INITIALS = "SCC de Snaijer";
-export const REVALIDATE_INTERVAL = 3600; // total seconds = 1 hour
+export const REVALIDATE_INTERVAL = 86400; // total seconds = 24 hours
