75665de8e9829f39b1ad8fe1ff7d3996ff00502f
Sander de Snaijer <sanderdesnaijer@gmail.com>
2026-05-06 20:59:06 +0700

    fix: test

--- app/(pages)/tags/[slug]/not-found.tsx
+++ app/(pages)/tags/[slug]/not-found.tsx
@@ -1,7 +1,5 @@
 import { getTranslations } from "next-intl/server";
 import { NotFound } from "@/app/components/NotFound";
-import { pageSlugs } from "@/app/utils/routes";
-import { buildPageUrl } from "@/app/utils/utils";
 
 export async function generateMetadata() {
   const t = await getTranslations();
@@ -23,7 +21,7 @@ export default async function TagNotFound() {
     <NotFound
       title={t("error.404.tag.title")}
       description={t("error.404.tag.description")}
-      href={buildPageUrl(pageSlugs.tags)}
+      href="/tags"
       action={t("error.404.tag.action")}
     />
   );
